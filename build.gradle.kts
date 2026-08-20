97c1b947b714d8ce7839e732ea5a21117d741f57
ControlCenterSoft <40738562+ControlCenterSoft@users.noreply.github.com>
2026-08-20 12:08:51 +0300

    Configure Android SDK build plugins

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
+    }
+}
+
+plugins {
+    id("com.android.library") version "9.3.0" apply false
+}
